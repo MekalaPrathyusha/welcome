44d35ed3cecf529ac6d94852a7ad033e489c0a4c
MekalaPrathyusha <prathyu595@gmail.com>
2017-04-27 15:17:22 +0530

    Spring framework
    Printing Statement

--- src/main/java/com/niit/welcome/App.java
+++ src/main/java/com/niit/welcome/App.java
@@ -11,7 +11,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello" );
         
         ApplicationContext context=new FileSystemXmlApplicationContext("beans.xml");
         WelcomeMessage messageBean=(WelcomeMessage)context.getBean("welcomeMessageBean");
